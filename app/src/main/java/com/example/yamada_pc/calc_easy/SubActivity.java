6d5cf1fe670e3588cc1b0516fc02bfd8c01e1616
t-YAMADA <16043065@mmm.muroran-it.ac.jp>
2016-12-03 21:01:33 +0900

    check screen transition

---
+++ app/src/main/java/com/example/yamada_pc/calc_easy/SubActivity.java
@@ -0,0 +1,17 @@
+package com.example.yamada_pc.calc_easy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by yamada-PC on 2016/12/03.
+ */
+
+public class SubActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sub);
+    }
+}
